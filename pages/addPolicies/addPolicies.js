45d890671f7d68f5fe4490f0ceef177d5a91367e
Michael Garvie <michael@fastcomm.com>
2020-08-12 08:30:55 +0200

    soon to be done with the flows

--- pages/addPolicies/addPolicies.js
+++ pages/addPolicies/addPolicies.js
@@ -17,4 +17,22 @@ Page({
     this.data.user.policies = app.user.policies;
     this.data.user.devices = app.user.devices;
   },
+  onDeviceInsuranceCardClick: function(ev) {
+    my.navigateTo({ url: '../deviceInsurance/deviceInsurance' });
+    },
+  onFuneralCoverCardClick: function(ev) {
+    my.navigateTo({ url: '../funeralCover/funeralCover' });
+    },
+  onLifeCoverCardClick: function(ev) {
+    my.navigateTo({ url: '../lifeCover/lifeCover' });
+    },
+  onLegalCoverCardClick: function(ev) {
+    my.navigateTo({ url: '../legalCover/legalCover' });
+    },
+  onHeroAssistCardClick: function(ev) {
+     my.navigateTo({ url: '../heroAssist/heroAssist' });
+  },
+  onDialATeacherCardClick: function(ev) {
+    my.navigateTo({ url: '../dialATeacher/dialATeacher' });
+    },
 });
